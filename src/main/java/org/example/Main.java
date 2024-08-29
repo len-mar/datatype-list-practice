fe81582864ccf0947b25466a26050dfb6324a98f
marleneanton <the.marlene.anton@gmail.com>
2024-08-29 20:55:12 +0200

    adds sack and modifies main to demonstrate all sack methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,27 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Sack sack = new Sack();
+        sack.add(0);
+        sack.add(1);
+        sack.add(2);
+        sack.add(3);
+        sack.add(4);
+
+        System.out.println("Let's print our sack. " + sack);
+
+
+        System.out.println("What's in our sack at index 1? " + sack.get(1));
+        System.out.println("What's in our sack at index 4? " + sack.get(4));
+
+        System.out.println("How many ints are in our sack? " + sack.size());
+
+        sack.removeLast();
+        System.out.println("We've removed the last element from our sack. Now, how many are there? " + sack.size());
+
+        sack.removeByIndex(1);
+        System.out.println("We've removed the element at index 1 from our sack. Now, how many are there? " + sack.size());
+
+        System.out.println("Let's print our sack again. " + sack);
     }
 }
--- src/main/java/org/example/Sack.java
+++ src/main/java/org/example/Sack.java
@@ -1,135 +1,63 @@
+// Sack built for ints only - additional data types would require:
+// copy/pasting all fields, constructors, and methods, one for each data type
 package org.example;
-import java.lang.reflect.Array;
-import java.util.Iterator;
-import java.util.Spliterator;
-import java.util.function.Consumer;
+import java.util.Arrays;
 
-// TODO: what is an arraylist?
-// an iterable class
-// capable of holding a finite (but adjustable) number of elements
-// these elements are zero-indexed
-// and can be deleted, found, added, while keeping with the indices
+public class Sack {
+    private int[] array;
 
-// TODO: our specific arraylist:
-// needs to be able to hold arrays and primitives
-// so ints, shorts, doubles, floats, longs, booleans,chars, bytes?
-
-public class Sack <T>{
-    public int index = 0;
-    // I can think of no other way than manipulating arrays for this
-    public int[] ints;
-    public double[] doubles;
-    public float[] floats;
-    public byte[] bytes;
-    public long[] longs;
-    public boolean[] booleans;
-    public short[] shorts;
-    public char[] chars;
-    public Array[] arrays;
-    private T[] array;
-
-
-// since there are nine primitive data types, we need 9 constructors?
-    public Sack(Array[] array) {
-        this.arrays = array;
-    }
-    public Sack(int[] ints) {
-        this.ints = ints;
-    }
-    public Sack(double[] doubles) {
-        this.doubles = doubles;
-    }
-    public Sack(float[] floats) {
-        this.floats = floats;
-    }
-    public Sack(byte[] bytes) {
-        this.bytes = bytes;
-    }
-    public Sack(long[] longs) {
-        this.longs = longs;
-    }
-    public Sack(boolean[] booleans) {
-        this.booleans = booleans;
-    }
-    public Sack(short[] shorts) {
-        this.shorts = shorts;
-    }
-    public Sack(char[] chars) {
-        this.chars = chars;
+    public Sack() {
+        this.array = new int[0];
     }
 
-
-    // TODO: will some methods only work for the primitive or the array arrays?
-    // let's see
-
-    // TODO; how to add regardless of data type? -generics?
-
-//    public void add(T toBeAdded){
-//        T[] temp = new T[this.array.length+1];
-//        for(int i = 0; i < this.array.length; i++){
-//            temp[i] = this.array[i];
-//        }
-//        temp[this.array.length] = toBeAdded;
-//        this.array = temp;
-//    }
-
-    // yikes
-//    public void add(int toBeAdded){
-//        int[] temp = new int[this.ints.length+1];
-//        for(int i = 0; i < this.ints.length; i++){
-//            temp[i] = this.ints[i];
-//        }
-//        temp[this.ints.length] = toBeAdded;
-//        this.ints = temp;
-//    }
-    public void add(char toBeAdded){
-        char[] temp = new char[this.chars.length+1];
-        for(int i = 0; i < this.chars.length; i++){
-            temp[i] = this.chars[i];
+    public void add(int toBeAdded) {
+        int[] temp = new int[this.array.length + 1];
+        for (int i = 0; i < this.array.length; i++) {
+            temp[i] = this.array[i];
         }
-        temp[this.chars.length] = toBeAdded;
-    }
-    public void add(boolean toBeAdded){
-        boolean[] temp = new boolean[this.booleans.length+1];
-        for(int i = 0; i < this.booleans.length; i++){
-            temp[i] = this.booleans[i];
-        }
-        temp[this.booleans.length] = toBeAdded;
-    }
-    public void add(double toBeAdded){
-        double[] temp = new double[this.doubles.length+1];
-        for(int i = 0; i < this.doubles.length; i++){
-            temp[i] = this.doubles[i];
-        }
-        temp[this.doubles.length] = toBeAdded;
-    }
-    public void add(short toBeAdded){
-        short[] temp = new short[this.shorts.length+1];
-        for(int i = 0; i < this.shorts.length; i++){
-            temp[i] = this.shorts[i];
-        }
-        temp[this.shorts.length] = toBeAdded;
-    }
-    public void add(long toBeAdded){
-        long[] temp = new long[this.longs.length+1];
-        for(int i = 0; i < this.longs.length; i++){
-            temp[i] = this.longs[i];
-        }
-        temp[this.longs.length] = toBeAdded;
-    }
-    public void add(byte toBeAdded){
-        byte[] temp = new byte[this.bytes.length+1];
-        for(int i = 0; i < this.bytes.length; i++){
-            temp[i] = this.bytes[i];
-        }
-        temp[this.bytes.length] = toBeAdded;
-    }
-    public void add(float toBeAdded){
-        float[] temp = new float[this.floats.length+1];
-        for(int i = 0; i < this.floats.length; i++){
-            temp[i] = this.floats[i];
-        }
-        temp[this.floats.length] = toBeAdded;
+        temp[this.array.length] = toBeAdded;
+        this.array = temp;
     }
 
+    public int get(int index) {
+        return array[index];
+    }
+    public int size(){
+        return array.length;
+    }
+
+    public void removeLast(){
+        int[] temp = new int[array.length-1];
+        for(int i = 0; i < temp.length; i++){
+            temp[i] = array[i];
+        }
+        this.array = temp;
+    }
+
+    // removes element at index, replaces array with a new one that's one element shorter
+    public void removeByIndex(int index){
+        int[] temp = new int[array.length-1];
+        boolean found = false;
+        for(int i = 0; i < temp.length; i++){
+            // once at the index, set found to true
+            if(i == index){
+                found = true;
+                temp[i] = array[i+1];
+            }
+            // after index has been found, set values with an offset
+            if(found) temp[i] = array[i + 1];
+
+            // if not found yet, replace array 1:1
+            else temp[i] = array[i];
+        }
+
+        // finally, replace array with the fresh temp array
+        this.array = temp;
+    }
+
+    @Override
+    public String toString() {
+        return "Sack: " + Arrays.toString(array) +
+                '.';
+    }
 }
